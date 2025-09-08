0a5ecb96c20875ee9504676ee6bab8396b118dd5
shhriiii <149945420+shhriiii@users.noreply.github.com>
2025-09-08 23:11:39 +0530

    Time: 63 ms (16.44%) | Memory: 159.2 MB (7.06%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    int helper(int n, int screenVisible, int buffer, int flag) {
-        if (screenVisible == n) return 0;
-        if (screenVisible > n) return 1e9;
+    vector<vector<int>> t;
 
-        if (screenVisible == n) {
-            return 0;
+    int solve(int currCountA, int pasteCountA, int n) {
+        if (currCountA == n) return 0;
+        if (currCountA > n) return 1000;
+
+        if (t[currCountA][pasteCountA] != -1) {
+            return t[currCountA][pasteCountA];
         }
 
-        int copy = 1e9, paste = 1e9;
-        if (!flag) {
-            copy = 1 + helper(n, screenVisible, screenVisible, 1);
+        int copyPaste = 2 + solve(currCountA + currCountA, currCountA, n);
+        int paste = 1e9;
+        if (pasteCountA > 0) {
+            paste = 1 + solve(currCountA + pasteCountA, pasteCountA, n);
         }
-        if (buffer > 0) {
-            paste = 1 + helper(n, screenVisible + buffer, buffer, 0);
-        }
-        return min(copy, paste);
 
-        return min(copy, paste);
+        return t[currCountA][pasteCountA] = min(copyPaste, paste);
     }
-    int minSteps(int n) {
-        if (n == 1)
-            return 0;
-        if (n >= 2 && n <= 5)
-            return n;
 
-        return helper(n, 1, 0, 0);
+    int minSteps(int n) {
+        if (n == 1) return 0;
+        t.assign(n + 1, vector<int>(n + 1, -1));
+        return 1 + solve(1, 1, n);
     }
 };
