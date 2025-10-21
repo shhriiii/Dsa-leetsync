8d24d494f51074aaf1adf57942129826dcc5412c
shhriiii <149945420+shhriiii@users.noreply.github.com>
2025-10-22 01:08:05 +0530

    Time: 598 ms (23.66%) | Memory: 181.6 MB (27.56%) - LeetSync

---
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -0,0 +1,49 @@
+
+
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int no) {
+        int n = nums.size();
+        
+        // 1. Store original frequencies
+        unordered_map<int, int> freq;
+        for (int x : nums) {
+            freq[x]++;
+        }
+
+
+        sort(nums.begin(), nums.end());
+
+        // 3. Create a set of all "critical" candidate targets.
+        // We use long long to avoid overflow when adding/subtracting k.
+        set<long long> candidates;
+        for (int x : nums) {
+            candidates.insert((long long)x);
+            candidates.insert((long long)x - k);
+            candidates.insert((long long)x + k);
+        }
+
+        int mxm = 0; 
+        for (long long T : candidates) {
+            long long lhs = T - k;
+            long long rhs = T + k;
+            auto it_r = upper_bound(nums.begin(), nums.end(), rhs);
+            auto it_l = lower_bound(nums.begin(), nums.end(), lhs);
+
+            int total_changeable = it_r - it_l;
+
+            int C1 = 0;
+            if (T >= INT_MIN && T <= INT_MAX && freq.count((int)T)) {
+                C1 = freq.at((int)T);
+            }
+            int C2 = total_changeable - C1;
+
+
+            int final_freq = C1 + min(no, C2);
+
+            mxm = max(mxm, final_freq);
+        }
+
+        return mxm;
+    }
+};
