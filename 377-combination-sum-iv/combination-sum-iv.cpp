022970811d5806abf0a876f5168d56383250ace7
shhriiii <149945420+shhriiii@users.noreply.github.com>
2025-11-06 23:21:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (21.40%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    int n;
-    vector<vector<int>> dp;
-
-    int helper(vector<int>& nums, int target , int currSum , int idx){
-        if(currSum > target || idx >= n) return 0;
-        if(currSum == target) return 1;
-
-        if(dp[currSum][idx] != -1) return dp[currSum][idx];
-
-        int pick = helper(nums , target , currSum + nums[idx] , 0);
-
-        int notPick = helper(nums , target , currSum , idx+1);
-
-        return dp[currSum][idx] = pick + notPick;
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> memo(target + 1, -1);
+        return helper(nums, target, memo);
     }
 
-    int combinationSum4(vector<int>& nums, int target) {
-        n = nums.size();
-        dp.assign(target + 1, vector<int>(n, -1));
-        return helper(nums , target , 0 , 0);
+    int helper(vector<int>& nums, int target, vector<int>& memo) {
+        if (target == 0) return 1;
+        if (target < 0) return 0;
+        if (memo[target] != -1) return memo[target];
+
+        int count = 0;
+        for (int num : nums) {
+            count += helper(nums, target - num, memo);
+        }
+        return memo[target] = count;
     }
 };
